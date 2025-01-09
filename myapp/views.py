3bafa41da2ea5dd502341a937353ce0c4c74818b
mridula3 <rumaiarahman3@gmail.com>
2025-01-09 16:25:31 +0600

    update 09.01.25

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import View
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import blog_post
 
 # Create your views here.
@@ -8,4 +8,8 @@ class Index(ListView):
     model = blog_post
     queryset = blog_post.objects.all().order_by('-date')
     template_name = 'index.html'
-    paginate_by = 2
+    paginate_by = 1
+
+class DetailArticleView(DetailView):
+    model = blog_post
+    template_name='blog_post.html'
