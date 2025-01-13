e24e489cd13e2fcb54dc4ebc0e4d60548000cf36
mridula3 <rumaiarahman3@gmail.com>
2025-01-13 14:49:57 +0600

    updated 13.01.25

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
-from .views import Index, DetailArticleView
+from .views import Index, DetailArticleView,LikeArticle,Featured
 
 urlpatterns = [
     path('', Index.as_view(), name='index'),
     path('tinymce/', include('tinymce.urls')),
     path('<int:pk>/', DetailArticleView.as_view(), name='detail_article'),
+    path('<int:pk>/like', LikeArticle.as_view(), name='like_article'),
+    path('featured/', Featured.as_view(), name='featured'),
+
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from django.views.generic import ListView, DetailView
 from .models import blog_post
@@ -10,6 +10,34 @@ class Index(ListView):
     template_name = 'index.html'
     paginate_by = 1
 
-class DetailArticleView(DetailView):
+class Featured(ListView):
     model = blog_post
+    queryset = blog_post.objects.filter(featured = True).order_by('-date')
+    template_name = 'featured.html'
+    paginate_by = 1
+
+class DetailArticleView(DetailView):
+    model = blog_post                           
     template_name='blog_post.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context= super(DetailArticleView, self).get_context_data(*args, **kwargs)
+        article = blog_post.objects.get(id=self.kwargs.get('pk'))
+        context['liked_by_user'] = False
+
+        if article.likes.filter(pk=self.request.user.id).exists():
+            context['liked_by_user'] = True
+            
+        return context 
+
+
+class LikeArticle(View):
+    def post(self, request, pk):
+        article = blog_post.objects.get(id=pk)
+        if article.likes.filter(pk=self.request.user.id).exists():
+            article.likes.remove(request.user.id)
+        else:
+            article.likes.add(request.user.id)
+
+        article.save()
+        return redirect('detail_article', pk)
